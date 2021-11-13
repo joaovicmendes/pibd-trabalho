f49f293e8e5866febc4426a03c80e38c9c165b94
amandapmn <40667726+amandapmn@users.noreply.github.com>
2021-11-13 20:55:56 -0300

    Create parte1.sql

---
+++ Trabalho 2/Etapa 4/parte1.sql
@@ -0,0 +1,68 @@
+-- Criando tabelas
+CREATE TABLE Endereco(
+    cep         VARCHAR2(9),
+    rua         VARCHAR2(128),
+    numero      NUMBER,
+    complemento VARCHAR2(128),
+    cidade      VARCHAR2(64) NOT NULL,
+    bairro      VARCHAR2(64) NOT NULL,
+
+    PRIMARY KEY (cep, rua, numero, complemento)
+);
+
+CREATE TABLE Carro(
+    placa  VARCHAR2(7) PRIMARY KEY,
+    ano    NUMBER NOT NULL,
+    modelo VARCHAR2(32) NOT NULL,
+    cor    VARCHAR2(32) DEFAULT 'Branco'
+);
+
+CREATE TABLE Pessoa(
+    codigo      NUMBER PRIMARY KEY,
+    nome        VARCHAR2(128) NOT NULL,
+    dataNasc    DATE NOT NULL,
+    homepage    VARCHAR2(128),
+    cep         VARCHAR2(9) NOT NULL,
+    rua         VARCHAR2(128) NOT NULL,
+    numero      NUMBER NOT NULL,
+    complemento VARCHAR2(128) NOT NULL,
+
+    FOREIGN KEY (cep, rua, numero, complemento) REFERENCES Endereco(cep, rua, numero, complemento)
+);
+
+CREATE TABLE Telefone(
+    codigo   NUMBER,
+    telefone VARCHAR2(16) NOT NULL,
+
+    FOREIGN KEY (codigo) REFERENCES Pessoa(codigo),
+    PRIMARY KEY (codigo, telefone)
+);
+
+CREATE TABLE Amizade(
+    codigo_pessoa1 NUMBER,
+    codigo_pessoa2 NUMBER,
+    dataAmizade DATE DEFAULT SYSDATE,
+
+    --oracle usa ON DELETE RESTRICT por padrão, portanto deve ser omitido
+    FOREIGN KEY (codigo_pessoa1) REFERENCES Pessoa(codigo),
+    FOREIGN KEY (codigo_pessoa2) REFERENCES Pessoa(codigo),
+    PRIMARY KEY (codigo_pessoa1, codigo_pessoa2)
+);
+
+CREATE TABLE Possui(
+    codigo NUMBER,
+    placa VARCHAR2(7),
+
+    FOREIGN KEY (codigo) REFERENCES Pessoa(codigo) ON DELETE CASCADE,
+    FOREIGN KEY (placa) REFERENCES Carro(placa) ON DELETE CASCADE,
+    PRIMARY KEY (codigo, placa)
+);
+
+---1. Crie uma sequência para o código da pessoa.
+
+CREATE SEQUENCE Pessoa_codigo_seq;
+
+---2. Altere a tabela da pessoa adicionando um atributo que conta o número de carros da pessoa, e adicionando um atributo que conta o número de amigos da pessoa. Ambos os atributos devem ser inteiros.
+
+ALTER TABLE Pessoa
+    ADD (num_carros NUMBER, num_amigos NUMBER);
