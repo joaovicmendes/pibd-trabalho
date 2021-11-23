f4af859e26f255461bb81f64badf7735cc896391
João Victor Mendes <mendesjoaovic@gmail.com>
2021-11-23 18:35:56 -0300

    tela de adicionar carro

--- Trabalho 2/Etapa 5/postgres/src/main/java/br/ufscar/dc/pibd/postgres/App.java
+++ Trabalho 2/Etapa 5/postgres/src/main/java/br/ufscar/dc/pibd/postgres/App.java
@@ -6,6 +6,9 @@ import java.awt.event.*;
 
 public class App {
     JFrame mainFrame;
+    JFrame secondaryFrame;
+    GridLayout gridLayout;
+    JButton addCancel;
 
     JPanel panel1;
     ButtonGroup buttonGroup;
@@ -23,16 +26,36 @@ public class App {
     JPanel panelButtons;
     JButton addCarro;
 
+    // Add Carro
+    JPanel carroPanel;
+    JLabel carroPlaca;
+    JTextField carroPlacaText;
+    JLabel carroAno;
+    JTextField carroAnoText;
+    JLabel carroModelo;
+    JTextField carroModeloText;
+    JLabel carroCor;
+    JTextField carroCorText;
+    JButton addCarroConfirm;
+
+    // Add Pessoa
+
     App() {
         // Layout principal
         mainFrame = new JFrame("Agenda Telefônica");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        secondaryFrame = new JFrame("Agenda Telefônica - Adicionar");
+        secondaryFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel1 = new JPanel();
         panel2 = new JPanel();
+        var onButtonClicked = new OnButtonClicked();
+        addCancel = new JButton("Cancelar");
+        addCancel.addActionListener(onButtonClicked);
 
         // MenuBar
         panelButtons = new JPanel();
         addCarro = new JButton("Adicionar Carro");
+        addCarro.addActionListener(onButtonClicked);
         panelButtons.add(addCarro);
 
         // Botões para escolher se a busca é por Pessoa (código) ou Carro (placa)
@@ -49,7 +72,6 @@ public class App {
         labelTextField = new JLabel("Código ou Placa");
         textField = new JTextField(10);
         querySend = new JButton("Buscar");
-        var onButtonClicked = new OnButtonClicked();
         querySend.addActionListener(onButtonClicked);
         textArea = new JTextArea();
         textArea.setEditable(false);
@@ -68,8 +90,10 @@ public class App {
         mainFrame.getContentPane().add(BorderLayout.NORTH, panel1);
         mainFrame.getContentPane().add(BorderLayout.CENTER, panel2);
         mainFrame.getContentPane().add(BorderLayout.SOUTH, textArea);
-        mainFrame.setSize(600, 400);
+        mainFrame.setSize(800, 600);
         mainFrame.setVisible(true);
+        secondaryFrame.setSize(800, 600);
+        secondaryFrame.setVisible(false);
     }
 
     class OnButtonClicked implements ActionListener {
@@ -87,7 +111,50 @@ public class App {
                 }
 
                 textArea.setText(result);
+            } else if (e.getSource() == addCancel) {
+                mainFrame.setVisible(true);
+                secondaryFrame.setVisible(false);
+                secondaryFrame.dispose();
+            } else if (e.getSource() == addCarro) {
+                setupAddCarro();
             }
         }
+    }
+
+    private void setupAddCarro() {
+        secondaryFrame.dispose();
+        gridLayout = new GridLayout(5, 2);
+
+        carroPanel = new JPanel();
+        carroPanel.setLayout(gridLayout);
+
+        carroPlaca = new JLabel("Placa");
+        carroPlacaText = new JTextField(7);
+        carroPanel.add(carroPlaca);
+        carroPanel.add(carroPlacaText);
+
+        carroAno = new JLabel("Ano");
+        carroAnoText = new JTextField(4);
+        carroPanel.add(carroAno);
+        carroPanel.add(carroAnoText);
+
+        carroModelo = new JLabel("Modelo");
+        carroModeloText = new JTextField(25);
+        carroPanel.add(carroModelo);
+        carroPanel.add(carroModeloText);
+
+        carroCor = new JLabel("Cor");
+        carroCorText = new JTextField(25);
+        carroPanel.add(carroCor);
+        carroPanel.add(carroCorText);
+
+        addCarroConfirm = new JButton("Adicionar");
+        carroPanel.add(addCancel);
+        carroPanel.add(addCarroConfirm);
+
+        secondaryFrame.getContentPane().add(BorderLayout.CENTER, carroPanel);
+
+        secondaryFrame.setVisible(true);
+        mainFrame.setVisible(false);
     }
 }
