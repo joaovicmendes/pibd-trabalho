1e62064913f925b7fd35da34ec85d9f12fda3f97
amandapmn <40667726+amandapmn@users.noreply.github.com>
2021-11-23 20:16:53 -0300

    Cadastrar Pessoas

---
+++ Trabalho 2/Etapa 5/postgres/src/main/java/br/ufscar/dc/pibd/postgres/InsertPessoa.java
@@ -0,0 +1,36 @@
+package br.ufscar.dc.pibd.postgres;
+
+import java.sql.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class InsertPessoa {
+    static Connection connection;
+    static PreparedStatement statement;
+
+    static String query =
+            "INSERT INTO Pessoa(codigo, nome, dataNasc, homepage, cep, rua, numero, complemento) VALUES (?,?,?,?);";
+
+
+    public static String insert(String codigo, String nome, String data, String homepage, String cep, String rua, int numero, String complemento ) {
+        try {
+            connection = DriverManager.getConnection(
+                    Utils.DatabaseUrl, Utils.DatabaseUser, Utils.DatabasePassword);
+            statement = connection.prepareStatement(query);
+
+            statement.setString(1, Integer.parseInt(codigo));
+            statement.setString(2, nome);
+            statement.setInt(3, SimpleDateFormat("dd/MM/yyyy").parse(data));
+            statement.setString(4, homepage);
+            statement.setString(5, cep);
+            statement.setString(6, rua);
+            statement.setString(7, Integer.parseInt(numero));
+            statement.setString(5, complemento);
+
+            statement.executeUpdate();
+            return "Pessoa inserida";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+}
