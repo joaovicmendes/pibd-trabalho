ca3eab0077a0387eef5d90c08e14c7b2d5a78a78
amandapmn <40667726+amandapmn@users.noreply.github.com>
2021-11-24 09:41:00 -0300

    Create InsertEndereco.java

---
+++ Trabalho 2/Etapa 5/postgres/src/main/java/br/ufscar/dc/pibd/postgres/InsertEndereco.java
@@ -0,0 +1,34 @@
+package br.ufscar.dc.pibd.postgres;
+
+import java.sql.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class InsertEndereco {
+    static Connection connection;
+    static PreparedStatement statement;
+
+    static String query =
+            "INSERT INTO Endereco(cep, rua, numero, complemento, cidade, bairro) VALUES (?,?,?,?,?,?);";
+
+
+    public static String insert(String cep, String rua, String numero, String complemento, String cidade, String bairro ) {
+        try {
+            connection = DriverManager.getConnection(
+                    Utils.DatabaseUrl, Utils.DatabaseUser, Utils.DatabasePassword);
+            statement = connection.prepareStatement(query);
+
+            statement.setString(1, cep);
+            statement.setString(2, rua);
+            statement.setInt(3, Integer.parseInt(numero));
+            statement.setString(4, complemento);
+            statement.setString(5, cidade);
+            statement.setString(6, bairro);
+
+            statement.executeUpdate();
+            return "Endereço inserido";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+}
